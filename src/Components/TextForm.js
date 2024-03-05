1b0369ae72071da0851fac737ba27d46c76a6cf3
Ishika-45 <bansalishika58@gmail.com>
2024-03-05 23:02:54 +0530

    Make Uppercase

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,22 +1,23 @@
 import React, {useState} from 'react'
 
-
-const [text, setText] = useState('Enter text here');
-
 export default function TextForm(props) {
+  const handleUpClick = ()=>{
+    console.log("Uppercase was clicked " + text);
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+  const handleOnChange = (event)=>{
+    console.log("On change");
+    setText(event.target.value);
+  }
+  const [text, setText] = useState('Enter text here');
   return (
     <div>
       <div className="mb-3">
-        <label for="FormControl" className="form-label"
-        >Enter your text</label
-        >
-        <textarea
-          className="form-control"
-          id="FormControl"
-          rows="5"
-        ></textarea>
+        <label for="FormControl" className="form-label">Enter your text</label>
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="FormControl" rows="5"></textarea>
       </div>
-      <button className="btn btn-primary">UpperCase</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>UpperCase</button>
     </div>
   )
 }
