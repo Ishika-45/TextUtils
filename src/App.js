2bfe35581643717afd39a36929672d093679cd86
Ishika-45 <bansalishika58@gmail.com>
2024-05-07 21:02:30 +0530

    Dark mode

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
-// import PropTypes from 'prop-types'
-// import TextForm from './Components/TextForm';
+import PropTypes from 'prop-types'
+import TextForm from './Components/TextForm';
 import About from './Components/About';
+import React, { useState } from 'react';
 
 
 
 function App() {
+  const [mode, setMode] = useState('dark');
   return (
     <>
       {/* <Navbar title = "Electronio"/> */}
-      <Navbar />
-      {/* <div className="container my-3">
+      <Navbar title = "TextUtils" mode = {mode}/>
+      <div className="container my-3 ">
         <TextForm />
-      </div> */}
+      </div>
       <About/>
     
       
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className={'navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}'}>
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,10 +27,10 @@ export default function Navbar(props) {
                             </ul>
                         </li>
                     </ul>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-warning" type="submit">Search</button>
-                    </form>
+                    <div className = "form-check form-switch">
+  <input className = "form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label className = "form-check-label" htmlFor="flexSwitchCheckDefault">Default switch checkbox input</label>
+</div>
                 </div>
             </div>
         </nav>
