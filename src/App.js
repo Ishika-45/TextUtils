14d65168396cfb19cb5873941d8f1da2c3219825
Ishika-45 <bansalishika58@gmail.com>
2024-05-09 12:26:33 +0530

    Edit title and Favicon

--- src/App.js
+++ src/App.js
@@ -5,30 +5,49 @@ import PropTypes from 'prop-types'
 import TextForm from './Components/TextForm';
 // import About from './Components/About';
 import React, { useState } from 'react';
+import Alert from './Components/Alert';
 
 
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+
+  }
 
   const toggleMode = ()=>{
     if(mode === 'light'){
       setMode('dark');
-      document.body.style.backgroundColor = 'grey';
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark mode has been enabled","success");
   }
   else{
     setMode('light');
     document.body.style.backgroundColor = 'white';
+    showAlert("Light mode has been enabled","success");
+    console.log("toggle mode has been clicked");
   }
   }
   return (
     <>
       {/* <Navbar title = "Electronio"/> */}
       <Navbar title = "TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+
       <div className="container my-3 ">
-        <TextForm mode={mode} />
+        <TextForm showAlert={showAlert} mode={mode} />
       </div>
       {/* <About/> */}
+      
     
       
     </>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -5,6 +5,7 @@ export default function TextForm(props) {
     console.log("Uppercase was clicked ");
     let newText = text.toUpperCase();
     setText(newText)
+    props.showAlert("Converted to uppercase","success");
   }
   const handleOnChange = (event) => {
     console.log("On change");
@@ -14,22 +15,26 @@ export default function TextForm(props) {
     console.log("Lowercase was clicked ");
     let newText = text.toLowerCase();
     setText(newText)
+    props.showAlert("Converted to lowercase","success");
   }
   const handleCapitalize = () => {
     console.log("Capitalize was clicked ");
     let newText = text.charAt(0).toUpperCase() + text.slice(1);
     setText(newText)
+    props.showAlert("Converted to Capitalize","success");
   }
   const handleCopy = () => {
     console.log("Copy was clicked ");
     var newText = document.getElementById("FormControl");
     newText.select();
     navigator.clipboard.writeText(newText.value);
+    props.showAlert("Copied to Clipboard","success");
   }
   const handleClear = () => {
     console.log("Clear was clicked ");  
     let newText = '';
     setText(newText)
+    props.showAlert("Text Cleared","danger");
   }
 
   const [text, setText] = useState('Enter text here');
@@ -38,7 +43,7 @@ export default function TextForm(props) {
       <div className="container">
         <div className={`mb-3 text-${props.mode==='light'?'dark':'light'}`}>
           <h1>Enter your text</h1>
-          <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'light':'grey'}}id="FormControl" rows="5"></textarea>
+          <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='light'?'light':'grey', color: props.mode==='light'?'black':'light'}} id="FormControl" rows="5"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>UPPERCASE</button>
         <button className="btn btn-primary mx-2" onClick={handleLowClick}>lowercase</button>
